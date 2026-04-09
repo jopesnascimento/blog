463ccff86d90f480913a6e5ecfc02936b12a2e35
jopesnascimento <jopesouzanascimento@gmail.com>
2026-04-08 23:35:28 -0300

    feat: atualizando e corrigindo os bugs

--- app.py
+++ app.py
@@ -65,7 +65,6 @@ def home_page():
     posts = list(colecao.find().sort("_id", -1))
     return render_template("index.html", posts=posts, logado=usuario_logado())
 
-
 @app.route("/post/<post_id>")
 def post_detalhes(post_id):
     post, erro = buscar_post(post_id)
